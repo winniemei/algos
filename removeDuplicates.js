9d02dfa8f42ee2d583e9826afb186a8e391b2853
winniemei <wmei91@gmail.com>
2023-10-28 19:21:05 -0400

    solved removeDuplicates

---
+++ removeDuplicates.js
@@ -0,0 +1,24 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+	count = 0;
+	const newObj = {};
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] in newObj) {
+			newObj[nums[i]] += 1;
+		} else {
+			newObj[nums[i]] = 1;
+			nums[count++] = nums[i];
+		}
+	}
+	return count;
+};
